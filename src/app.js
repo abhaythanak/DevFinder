e937726b9f25fe5a39f6dec9635e784c217bf114
abhaythanak <abhaythanak@gmail.com>
2025-07-11 16:50:49 +0530

    exploring get,post,delete methods

--- src/app.js
+++ src/app.js
@@ -5,6 +5,16 @@ const app = express();
 // app.use("/", (req, res) => {
 //   res.send("hello 3000 server 12223");
 // });
+app.get("/user", (req, res) => {
+  res.send({ firstName: "abhay", lastName: "thanak" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("data added succesfully to database");
+});
+app.delete("/user", (req, res) => {
+  res.send("data deleted succesfully");
+});
 
 app.use("/hello", (req, res) => {
   res.send("hello 3000 server from route");
