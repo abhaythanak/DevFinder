cec4717542de5a770a73449072dbfa15e3a42dea
abhaythanak <abhaythanak@gmail.com>
2025-07-20 18:52:08 +0530

    /user/connections it connection is accepted API

--- src/routes/user.js
+++ src/routes/user.js
@@ -21,3 +21,29 @@ useRouter.get("./user/requests/received", userAuth, async (req, res) => {
     res.status(400).send({ message: error.message });
   }
 });
+
+useRouter.get("./user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("TOUserId", USER_SAFE_DATA);
+
+    console.log(connectionRequest);
+
+    const data = connectionRequest.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.json({ data });
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
