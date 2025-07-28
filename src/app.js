73ff7d59fa07399bc16cf8a71b44388959847b95
abhaythanak <abhaythanak@gmail.com>
2025-07-29 00:07:24 +0530

    cors origin access  and credential implement

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ const requestRouter = require("./routes/request");
 const useRouter = require("./routes/user");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
