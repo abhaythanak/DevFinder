12a152fe29707d7c55cccea183a86764ca66da3d
abhaythanak <abhaythanak@gmail.com>
2025-08-02 21:48:50 +0530

    sending the user data as response when login successfull

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -22,7 +22,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
       });
-      res.send("Login Successful !!!");
+      res.send(user);
     }
   } catch (error) {
     res.status(400).send("error saving the user:" + error.message);
